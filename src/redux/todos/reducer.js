f3cb7e6c4c56d8de45b59c2437f874fcf910c7f6
Muhammad Anisuzzaman <anisseam238@gmail.com>
2022-08-24 20:51:19 +0600

    Create Reducer Functions

---
+++ src/redux/todos/reducer.js
@@ -0,0 +1,79 @@
+import {
+  ADDED,
+  ALLCOMPLETED,
+  CLEARCOMPLETED,
+  COLORSELECTED,
+  DELETED,
+  TOGGLED,
+} from "./actionTypes";
+
+const initialState = [
+  {
+    id: 1,
+    text: "Learn React JS",
+    completed: true,
+  },
+  {
+    id: 2,
+    text: "Learn Redux",
+    completed: false,
+    color: "red",
+  },
+];
+
+const nextTodoId = (todos) => {
+  const maxId = todos.reduce((maxi, todo) => Math.max(maxi, todo.id), -1);
+  return maxId + 1;
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADDED:
+      return [
+        ...state,
+        {
+          id: nextTodoId(state),
+          text: action.payload,
+          completed: false,
+        },
+      ];
+
+    case TOGGLED:
+      return state.map((todo) => {
+        if (todo.id == action.payload) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return { ...todo };
+      });
+
+    case COLORSELECTED:
+      return state.map((todo) => {
+        if (todo.id == action.payload.todoId) {
+          return {
+            ...todo,
+            color: action.payload.color,
+          };
+        }
+        return { ...todo };
+      });
+
+    case DELETED:
+      return state.filter((todo) => todo.id !== action.payload);
+
+    case ALLCOMPLETED:
+      return state.map((todo) => {
+        return { ...todo, completed: true };
+      });
+
+    case CLEARCOMPLETED:
+      return state.filter((todo) => !todo.completed);
+
+    default:
+      return state;
+  }
+};
+
+export default reducer;
